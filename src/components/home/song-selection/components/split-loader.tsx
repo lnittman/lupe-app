b0b3c0711cd1c159af0e8a543d10025900b219bf
Luke Nittmann <luke.nittmann@gmail.com>
2025-02-13 22:56:23 -0800

    remove unused useRef

--- src/components/home/song-selection/components/split-loader.tsx
+++ src/components/home/song-selection/components/split-loader.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from "framer-motion";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 
 import { useAudioStore } from "@/store";
 import { StemType } from "@/types/audio";
